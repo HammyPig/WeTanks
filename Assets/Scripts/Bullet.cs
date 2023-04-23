3595453ec24c6bc8310a9f728d9adb14c0f097c3
HammyPig <jamesbwalden@protonmail.com>
2023-04-23 23:17:31 +1000

    add bullet and wall collision mechanics

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
+    public int bounceLimit;
+    private int bounceCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bounceCount = 0;
     }
 
     // Update is called once per frame
@@ -18,5 +20,15 @@ public class Bullet : MonoBehaviour
         Vector2 velocity = rigidBody.velocity;
         float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall")) {
+            if (bounceCount >= bounceLimit) {
+                Destroy(gameObject);
+            }
+
+            bounceCount += 1;
+        }
     }
 }
