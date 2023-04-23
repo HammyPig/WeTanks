149ffddf7f4051998a083652c97b6a574e0bf34d
HammyPig <jamesbwalden@protonmail.com>
2023-04-23 18:28:49 +1000

    add tank bullet firing

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Tank : MonoBehaviour
 {
     public float speed = 2f;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,15 @@ public class Tank : MonoBehaviour
     {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
         transform.Translate(speed * direction * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mouseDirection = (mousePosition - transform.position).normalized;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+
+            bulletRb.velocity = bulletSpeed * mouseDirection;
+        }
     }
 }
