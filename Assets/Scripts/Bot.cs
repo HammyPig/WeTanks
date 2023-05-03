90cb68ac0ebc40930413757a770a853e7cc47c5b
HammyPig <jamesbwalden@protonmail.com>
2023-05-03 14:53:42 +1000

    add bot detecting line of sight with player

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : Controller
+{
+    public GameObject target;
+    public LayerMask wallLayer;
+
+    void Start() {
+        wallLayer = LayerMask.GetMask("Wall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.transform.position - transform.position, Mathf.Infinity, wallLayer);
+        if (hit.collider != null) {
+            Debug.Log("cannot see tank");
+        } else {
+            Debug.Log("can see tank");
+        }
+    }
+}
