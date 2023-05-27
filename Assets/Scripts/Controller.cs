754b92bc209e0aaad9d542d432fba56d404b4d4e
Anna R <annar@192-168-1-153.tpgi.com.au>
2023-05-27 12:52:43 +1000

    add rooms and ability for player to move their own tank

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public abstract class Controller : MonoBehaviour
 {
+    protected   PhotonView view;
     protected Tank tank;
 
     // Start is called before the first frame update
     protected virtual void Start()
     {
         tank = gameObject.GetComponent<Tank>();
+        view = GetComponent<PhotonView>();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,27 +7,30 @@ public class Player : Controller
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if(view.IsMine)
+        {
+            Vector2 movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        float currentAngle = tank.transform.rotation.eulerAngles.z;
-        float targetAngle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
-        float deltaAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
+            float currentAngle = tank.transform.rotation.eulerAngles.z;
+            float targetAngle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            float deltaAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
 
-        float throttle = movementDirection.magnitude * (Mathf.Abs(deltaAngle) > 90 ? -1 : 1);
-        tank.throttle(throttle);
+            float throttle = movementDirection.magnitude * (Mathf.Abs(deltaAngle) > 90 ? -1 : 1);
+            tank.throttle(throttle);
 
-        if (throttle > 0) {
-            tank.rotateTowards(targetAngle);
-        } else if (throttle < 0) {
-            tank.rotateTowards(targetAngle + 180);
-        }
-        
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 mouseDirection = (mousePosition - tank.transform.position).normalized;
-        tank.rotateTurretTowards(mouseDirection);
+            if (throttle > 0) {
+                tank.rotateTowards(targetAngle);
+            } else if (throttle < 0) {
+                tank.rotateTowards(targetAngle + 180);
+            }
+            
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mouseDirection = (mousePosition - tank.transform.position).normalized;
+            tank.rotateTurretTowards(mouseDirection);
 
-        if (Input.GetMouseButtonDown(0)) {
-            tank.shoot();
+            if (Input.GetMouseButtonDown(0)) {
+                tank.shoot();
+            }
         }
     }
 }
